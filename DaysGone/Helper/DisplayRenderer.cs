54ac7aa0535076e462df09fd62a533a70cefecc6
Pawana McKay <pawana.schirkeinst@googlemail.com>
2025-04-25 10:48:14 +0200

    WIP Rework

--- DaysGone/Helper/DisplayRenderer.cs
+++ DaysGone/Helper/DisplayRenderer.cs
@@ -3,6 +3,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using StardewModdingAPI;
 using StardewValley;
+using StardewValley.Menus;
 using System.Runtime.CompilerServices;
 
 namespace Days_Gone.Helper;
@@ -11,11 +12,13 @@ public static class DisplayRenderer
 {
     private static Texture2D _scrollTexture;
     private static Texture2D _textBoxTexture;
+    private static Rectangle _textBoxSourceRect;
 
     public static void LoadTextures(IModHelper helper)
     {
-        //_scrollTexture = helper.GameContent.Load<Texture2D>(Path.Combine(helper.DirectoryPath, "assets", "scroll.png"));
+        //_scrollTexture = helper.GameContent.Load<Texture2D>(@"LooseSprites\\scroll");
         _textBoxTexture = helper.GameContent.Load<Texture2D>(@"LooseSprites\\textBox");
+        _textBoxSourceRect = new Rectangle(0, 0, _textBoxTexture.Width, _textBoxTexture.Height);
     }
 
     public static void DrawDisplay(DaysGoneConfig config, string text, SpriteBatch spriteBatch)
@@ -41,7 +44,7 @@ public static class DisplayRenderer
     private static void DrawTextBox(SpriteBatch spriteBatch, Vector2 position, Vector2 textSize,
                                       string text, Color textColor, float opacity)
     {
-        // Create the Background
+        /*// Create the Background
         Rectangle background = new(
             (int)position.X - 10,
             (int)position.Y - 5,
@@ -63,13 +66,36 @@ public static class DisplayRenderer
         }
 
         // Draw the Text
-        spriteBatch.DrawString(Game1.smallFont, text, position, textColor);
+        spriteBatch.DrawString(Game1.smallFont, text, position, textColor);*/
+
+        int width = 300;
+        int height = (int)textSize.Y + 30;
+
+        IClickableMenu.drawTextureBox(
+            spriteBatch,
+            texture: _textBoxTexture,
+            sourceRect: _textBoxSourceRect,
+            x: (int)position.X - 15,
+            y: (int)position.Y - 10,
+            width: width,
+            height: height,
+            color: Color.White * opacity,
+            draw_layer: 1f,
+            drawShadow: false
+        );
+
+        Vector2 textPos = new Vector2(
+            position.X + (width - textSize.X) / 2 - 15,
+            position.Y
+        );
+
+        spriteBatch.DrawString(Game1.smallFont, text, textPos, textColor);
     }
 
     private static void DrawScroll(SpriteBatch spriteBatch, Vector2 position, Vector2 textSize,
                                      string text, Color textColor, float opacity)
     {
-        if (_scrollTexture == null) return;
+        /*if (_scrollTexture == null) return;
 
         // Adjust Scroll Size
         Rectangle scrollRect = new Rectangle(
@@ -85,6 +111,34 @@ public static class DisplayRenderer
             scrollRect.X + (scrollRect.Width - textSize.X) / 2,
             scrollRect.Y + (scrollRect.Height - textSize.Y) / 2);
 
-        spriteBatch.DrawString(Game1.smallFont, text, textPosition, textColor);
+        spriteBatch.DrawString(Game1.smallFont, text, textPosition, textColor);*/
+
+        if (_scrollTexture == null) return;
+
+        float scale = 0.8f;
+        Rectangle scrollRect = new Rectangle(
+            (int)position.X - (int)(_scrollTexture.Width * scale) / 2,
+            (int)position.Y - 10,
+            (int)(_scrollTexture.Width * scale),
+            (int)(_scrollTexture.Height * scale)
+        );
+
+        spriteBatch.Draw(
+            texture: _scrollTexture,
+            destinationRectangle: scrollRect,
+            sourceRectangle: null,
+            color: Color.White * opacity,
+            rotation: 0f,
+            origin: Vector2.Zero,
+            effects: SpriteEffects.None,
+            layerDepth: 1f
+        );
+
+        Vector2 textPos = new Vector2(
+            scrollRect.X + (scrollRect.Width - textSize.X) / 2,
+            scrollRect.Y + (scrollRect.Height - textSize.Y) / 2
+        );
+
+        spriteBatch.DrawString(Game1.smallFont, text, textPos, textColor);
     }
 }
